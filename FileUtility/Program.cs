0a6a9cc793080861af3e35748d506d441f141fc6
swong-wcd <swong@westcoastdental.com>
2022-04-18 14:14:04 -0700

    take home project from stephen wong

---
+++ FileUtility/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileUtility
+{
+    class Program
+    {
+        static string _DirectoryToTEST = $"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}test_data"; // For testing
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-index":
+                        if (args.Length >= 2)
+                            CreateIndex(args[1]);
+                        else
+                            Console.WriteLine("usage: FileUtility -index (folderPath)");
+                        break;
+                    case "-search":
+                        if (args.Length >= 4)
+                            Search(args[1], args[2], args[3]);
+                        else
+                            Console.WriteLine("usage: FileUtility -search (type) (value) (folderPath)");
+                        break;
+                }
+                
+            }
+            else
+                Console.WriteLine("usage: FileUtility [-index] or [-search (type) (value)]");
+        }
+
+        static void CreateIndex(string path)
+        {
+            var indexBuilder = new StringBuilder();
+            foreach (var filepath in Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories))
+            {
+                var fileInfo = new FileInfo(filepath);
+                if (fileInfo.Name != "files.txt")
+                    indexBuilder.AppendLine($"{fileInfo.Name}|{fileInfo.FullName}|{fileInfo.Length}|{fileInfo.Extension}");
+            }
+
+            var indexName = $"{path}{Path.DirectorySeparatorChar}files.txt";
+            using (var writer = new StreamWriter(File.Create(indexName, 8192, FileOptions.WriteThrough)))
+            {
+                writer.Write(indexBuilder.ToString());
+                writer.Flush();
+                writer.Close();
+            }
+
+            Console.WriteLine("index created");
+        }
+
+        static void Search(string type, string value, string path)
+        {
+            var index = InitSearchIndex(path, type);
+
+            var results = from x in index
+                          where x.Item1 == value
+                          select x.Item2;
+
+            if (results.Count() > 0)
+            {
+                foreach (var result in results)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            else
+                Console.WriteLine("No results found!");
+        }
+
+        static List<Tuple<string, string>> InitSearchIndex(string path, string searchType)
+        {
+            var index = new List<Tuple<string, string>>();
+
+            var indexName = $"{path}{Path.DirectorySeparatorChar}files.txt";
+            using (var reader = new StreamReader(File.OpenRead(indexName)))
+            {
+                bool isDone = false;
+                while (!isDone)
+                {
+                    var line = reader.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(line))
+                        isDone = true;
+                    else
+                    {
+                        var tokens = line.Split('|');
+
+                        switch (searchType)
+                        {
+                            case "name":
+                                var nameKey = tokens[0].Split('.')[0];
+                                index.Add(new Tuple<string, string>(nameKey, tokens[1]));
+                                break;
+                            case "size":
+                                index.Add(new Tuple<string, string>(tokens[2], tokens[1]));
+                                break;
+                            case "type":
+                                var typeKey = tokens[3].Substring(tokens[3].LastIndexOf('.') + 1);
+                                index.Add(new Tuple<string, string>(typeKey, tokens[1]));
+                                break;
+                        }
+                    }
+                }
+            }
+            return index;
+        }
+    }
+}
+ 
--- README.md
+++ README.md
@@ -2,14 +2,10 @@
 
 Challenge: A directory contains multiple files and directories of non-uniform file and directory names. Create a program that traverses a base directory and creates an index file that can be used to quickly lookup files by name, size, and content type.
 
-# Instructions
+# Usage
 
-Fork this repository and implement the above requirements. The result must be an application that implements both the index and search features. Use your best judgement as to the interface that is used to use the index and search features, but remember that this is meant to create a dialog during the interview process, not be something that would be used in production.
-
-Feel free to use the language, libraries, and tools that you are most comfortable in and best reflect your ability to translate requirements into a functional implementation.
-
-Once the project is implemented, remove the `# Instructions` section of this readme and add the section `# Usage` with instructions on how to run the produced application.
-
-The `test_data` directory in this project contains files and directories that can be used to test your implementation.
-
-Good luck.
+I used C# to create a console application (.exe) for this project. I committed the source code and the binaries to run this project. 
+To run the indexing, you can go to the net5.0 folder in either the debug or release folder and run this command -> [FileUtility -index (path of test_data)].
+To run the search, you can go to that same folder I mentioned earlier and run this command -> [FileUtility -search (name|size|type) (search term) (path of test_data)].
+I was able to run it on Windows using a command prompt so that I can see the output. 
+Thanks again for the opportunity.
